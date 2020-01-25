ee62a8a5387508d4f3ccaef61231f5e7a1bbe621
lamadipen <lamadipen@yahoo.com>
2020-01-25 13:59:55 -0800

    Lesson 2 Java Variables

--- JavaForBeginners/SampleApplication.java
+++ JavaForBeginners/SampleApplication.java
@@ -1,7 +1,12 @@
-
- public class SampleApplication{
+ public class SampleApplication{
 	
 	public static void main(String[] args){
-		System.out.println("Hello Welcome");
+		int amountByOwner = 10;
+		int amountToaAdd = 2;
+		int result = amountByOwner + amountToaAdd;
+
+		System.out.println("This is given by owner " + amountByOwner);
+		System.out.println("We added " + amountToaAdd);
+		System.out.println("Hello Welcome " + result);
 	}
 }
