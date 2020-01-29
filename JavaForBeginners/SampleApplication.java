3b3d811606fc251b4baed08aaf61fa4dcdd7bab2
lamadipen <lamadipen@yahoo.com>
2020-01-28 20:11:00 -0800

    Add Variable lesson 2 part2

--- JavaForBeginners/SampleApplication.java
+++ JavaForBeginners/SampleApplication.java
@@ -1,12 +1,22 @@
  public class SampleApplication{
-	
+ 	
+	static int testValue;
+
 	public static void main(String[] args){
-		int amountByOwner = 10;
-		int amountToaAdd = 2;
-		int result = amountByOwner + amountToaAdd;
+		float amountByOwner = 128.88f;
+		byte amountToaAdd = 2;
+		float result = amountByOwner + amountToaAdd;
+		boolean isTrue = true;
+		char a = 'A';
+		String apple = "apple";	
+		
+
+
+		System.out.println("this is test value "+ testValue);
 
 		System.out.println("This is given by owner " + amountByOwner);
 		System.out.println("We added " + amountToaAdd);
-		System.out.println("Hello Welcome " + result);
+		System.out.println("Hello Welcome " + result + "Is this true::" + isTrue);
+		System.out.println("This is char test " + a + " " + apple);
 	}
 }
