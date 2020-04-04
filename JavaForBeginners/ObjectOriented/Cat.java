42d80e2a8999de60d85bdbfac03ad2d0939f818f
lamadipen <lamadipen@yahoo.com>
2020-04-04 12:35:25 -0700

    Java Object and Class Encapsulation

---
+++ JavaForBeginners/ObjectOriented/Cat.java
@@ -0,0 +1,14 @@
+public class Cat{
+
+	String name;
+	String age;
+	String breed;
+
+	public void eat(){
+		System.out.println(this.name + " is eating");
+	}
+
+	public void run(){
+		System.out.println(this.name + " is running");
+	}
+}
--- JavaForBeginners/ObjectOriented/MainApplication.java
+++ JavaForBeginners/ObjectOriented/MainApplication.java
@@ -7,5 +7,19 @@ public class MainApplication{
 		dog.bark();
 		dog.eat();
 		dog.run();
+
+		Cat cat = new Cat();
+		cat.name = "some cat";
+		cat.eat();
+
+		Cat cat1 = new Cat();
+		cat1.name = "Random Cat";
+		cat1.eat();
+
+		Cat cat2 = new Cat();
+		cat2.name= "Black Cat";
+		cat2.eat();
+		
+
 	}
 }
