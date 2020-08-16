150c7fb7d4bfcb387550867e32dca20244d1f32b
lamadipen <lamadipen@yahoo.com>
2020-08-16 12:11:57 -0700

    Added Feature CRUD Product in online store

--- onlineStore/src/main/java/com/dipen/onlinestore/app/OnlineStoreApplication.java
+++ onlineStore/src/main/java/com/dipen/onlinestore/app/OnlineStoreApplication.java
@@ -2,14 +2,16 @@ package com.dipen.onlinestore.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class OnlineStoreApplication {
+
+	List<Product> products = new ArrayList<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineStoreApplication.class, args);
@@ -29,4 +31,47 @@ public class OnlineStoreApplication {
 	public String helloWithQueryParameter(@RequestParam String username, @RequestParam(name = "userage") int age){
 		return "Hello : " + username + "with age " + age;
 	}
+
+	@PostMapping("/msg")
+	public String getDataFromPost(@RequestBody String message) {
+		return "You have sent following message in Post: " + message;
+	}
+
+	@PutMapping("/msg")
+	public String getDataFromPut(@RequestBody String message) {
+		return "You have sent following message in Put: " + message;
+	}
+
+	@GetMapping("/product")
+	public List<Product> getAllProducts() {
+		return products;
+	}
+
+	@GetMapping("/product/{id}")
+	public Product getProductById(@PathVariable int id) {
+		for (int i = 0; i < products.size(); i++) {
+			if (products.get(i).getId() == id) {
+				return products.get(i);
+			}
+		}
+		return null;
+	}
+
+	@PostMapping("/product")
+	public String createProduct(@RequestBody Product product){
+		products.add(product);
+		return "product added successfully";
+	}
+
+	@DeleteMapping("/product/{id}")
+	public String deleteProduct(@PathVariable int id){
+		for (int i = 0; i < products.size(); i++) {
+			if (products.get(i).getId() == id) {
+				products.remove(i);
+				return "product deleted successfully";
+			}
+		}
+		return "product with id not found";
+	}
+
 }
